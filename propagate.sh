a6ca7721537680722fb5fe78e744d959ad5413b8
amalcew <par41lax@protonmail.com>
2026-05-11 19:25:30 +0200

    init.sh -> propagate.sh

--- propagate.sh
+++ propagate.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/bash
 
-# init.sh
-
-mkdir -p Targets/'Active Directory'/{dump,tools,exploits,logs}
-mkdir -p Targets/'Standalone no.1'/{dump,tools,exploits,logs}
-mkdir -p Targets/'Standalone no.2'/{dump,tools,exploits,logs}
-mkdir -p Targets/'Standalone no.3'/{dump,tools,exploits,logs}
-
 cp Checklists/'Target - Active Directory'/*.md Targets/'Active Directory'/'Host no.1'/
 cp Checklists/'Target - Active Directory'/*.md Targets/'Active Directory'/'Host no.2'/
 cp Checklists/'Target - Active Directory'/*.md Targets/'Active Directory'/'Host no.3'/
 cp Checklists/'Target - Active Directory'/Misc/* Targets/'Active Directory'/
-
 
 cp Checklists/'Target - Standalones'/*.md Targets/'Standalone no.1'/
 cp Checklists/'Target - Standalones'/Misc/* Targets/'Standalone no.1'/

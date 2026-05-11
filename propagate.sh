30e782a66d166a88ad2a9deb48d7c2b205004840
amalcew <par41lax@protonmail.com>
2026-05-11 19:28:56 +0200

    fix propagate.sh

--- propagate.sh
+++ propagate.sh
@@ -3,15 +3,15 @@
 cp Checklists/'Target - Active Directory'/*.md Targets/'Active Directory'/'Host no.1'/
 cp Checklists/'Target - Active Directory'/*.md Targets/'Active Directory'/'Host no.2'/
 cp Checklists/'Target - Active Directory'/*.md Targets/'Active Directory'/'Host no.3'/
-cp Checklists/'Target - Active Directory'/Misc/* Targets/'Active Directory'/
+cp -r Checklists/Misc/* Targets/'Active Directory'/
 
 cp Checklists/'Target - Standalones'/*.md Targets/'Standalone no.1'/
-cp Checklists/'Target - Standalones'/Misc/* Targets/'Standalone no.1'/
+cp -r Checklists/Misc/* Targets/'Standalone no.1'/
 
 cp Checklists/'Target - Standalones'/*.md Targets/'Standalone no.2'/
-cp Checklists/'Target - Standalones'/Misc/* Targets/'Standalone no.2'/
+cp -r Checklists/Misc/* Targets/'Standalone no.2'/
 
 cp Checklists/'Target - Standalones'/*.md Targets/'Standalone no.3'/
-cp Checklists/'Target - Standalones'/Misc/* Targets/'Standalone no.3'/
+cp -r Checklists/Misc/* Targets/'Standalone no.3'/
 
 tree

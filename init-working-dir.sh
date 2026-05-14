33667b0ef36842ff10e7418774aa3713fd3c6af9
amalcew <par41lax@protonmail.com>
2026-05-14 09:00:23 +0200

    tools/ dir moved

--- init-working-dir.sh
+++ init-working-dir.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-mkdir -p working/ad/{dump,exploits,logs,loot,tools}
-mkdir -p working/s1/{dump,exploits,logs,loot,tools}
-mkdir -p working/s2/{dump,exploits,logs,loot,tools}
-mkdir -p working/s3/{dump,exploits,logs,loot,tools}
+mkdir -p working/ad/{dump,exploits,logs,loot}
+mkdir -p working/s1/{dump,exploits,logs,loot}
+mkdir -p working/s2/{dump,exploits,logs,loot}
+mkdir -p working/s3/{dump,exploits,logs,loot}
+mkdir -p working/tools
